ddaa4d8bdad809da4b74bb0dbdeda773d532d574
Arnold <arnoldbkim@gmail.com>
2018-09-17 21:22:23 -0700

    update

--- models/index.js
+++ models/index.js
@@ -3,12 +3,16 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
+var basename  = path.basename(__filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
+console.log("Environment");
 console.log(env);
-console.log(process.env[config.use_env_variable]);
+console.log("Process environment vars");
+console.log(process.env);
+// console.log(process.env[config.use_env_variable]);
+console.log("Config");
 console.log(config);
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
