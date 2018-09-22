14c330ecbf0f2870083c0fb03df64c896b9a2441
Arnold <arnoldbkim@gmail.com>
2018-09-22 09:09:15 -0700

    update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,42 +1,23 @@
-
 var path = require("path");
 
 
-// Creating and exporting app to use on server.js
-module.exports = function (app) {
-  // Load activites.handlebars
-  app.get("/activities", function (req, res) {
-    db.Activity.findAll({
-      where: {
-        // column names from Activity Table: ajax request route
-        city: req.params.city,
-        category: req.params.category,
-        price: req.params.price
-      }
-    }).then(function (dbActivities) {
-      // Render in activities.handlebars
-      res.render("activities", {
-        // Giving activities property the res.json(dbActivities) from routes/apiRoutes.js
-        activities: dbActivities
-      });
-    });
-  });
+module.exports = function(app) {
+ // Load index page
+   app.get("/", function (req, res) {
+     res.render("index");
+   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
 
-  app.get("/tables", function(req, res) {
-    res.render("tables");
-  });
+ app.get("/res", function(req, res) {
+   res.render("reservations");
+ });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
+ app.get("/tables", function(req, res) {
+   res.render("tables");
+ });
+
+ // Render 404 page for any unmatched routes
+ app.get("*", function(req, res) {
+   res.render("404");
+ });
 };
